23382c83e217326d6197221378c6d8f601b17f61
Jesper Brink <jesper@brinkandersen.dk>
2019-02-27 11:59:43 +0100

    Added task-1 folder and word2vecf bash script: demo-word-txt.sh

---
+++ task-1/extract_vectors.py
@@ -0,0 +1,5 @@
+# Outlier skal bruge output fra w2v / w2vf - på samme form som sample_skipgram_wikipedia_vectors.txt
+import os
+
+if __name__ == "__main__":
+    os.system("cd ../word2vecf && ./demo-word-txt.sh")
